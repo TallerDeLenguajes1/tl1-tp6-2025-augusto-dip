a2f7a91e95b09b560e28bb3969925a260b68a18b
augusto-dip <augustodip26@gmail.com>
2025-06-03 14:16:49 -0300

    ejercicio 1

--- Program.cs
+++ Program.cs
@@ -1,9 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 
-int a;
-int b;
-a=10;
-b=a;
-Console.WriteLine("valor de a:"+a);
-Console.WriteLine("valor de b:"+b);
+//PROBANDO C#
+// int a;
+// int b;
+// a=10;
+// b=a;
+// Console.WriteLine("valor de a:"+a);
+// Console.WriteLine("valor de b:"+b);
+
+//EJERCICIO 1
+
+string texto = Console.ReadLine();
+int numero;
+bool esNum = int.TryParse(texto, out numero);
+
+if (esNum) {
+    if (numero > 0) {
+        int invertido = 0;
+        while(numero>0){
+            invertido = invertido * 10 + numero % 10;
+            numero = numero / 10;
+        }
+        Console.WriteLine("El numero invertido es " + invertido);
+    }
+    else {
+        Console.WriteLine("El numero ingresado no es mayor que cero");
+    }
+}
+else {
+    Console.WriteLine("El texto ingresado no es un numero entero");
+}
+
+
+
+
