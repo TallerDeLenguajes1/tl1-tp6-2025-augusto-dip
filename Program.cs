74d1bbd26a3353fffd60601df1410e0a56aacee0
augusto-dip <augustodip26@gmail.com>
2025-06-09 22:10:01 -0300

    ultimo punto con read modificado tambien

--- Program.cs
+++ Program.cs
@@ -109,57 +109,100 @@ Console.WriteLine("Hello, World!");
 //     }
 // }
 // EJERCICIO 3
+// float solicitarNumero()
+// {
+//     string numString = Console.ReadLine();
+//     float num;
 
-float solicitarNumero()
+//     while (!float.TryParse(numString, out num))
+//     {
+//         Console.Write("Ingrese un numero valido");
+//         numString = Console.ReadLine();
+//     }
+//     return num;
+// }
+// Console.Write("Ingrese un numero: ");
+// float num = solicitarNumero();
+
+// Console.WriteLine("|" + num + "| = " + Math.Abs(num));
+// Console.WriteLine(num + "^2 = " + num * num);
+// if (num >= 0)
+// {
+//     Console.WriteLine("Raiz cuadrada de " + num + " = " + Math.Sqrt(num));
+// }
+// else
+// {
+//    Console.WriteLine("No podemos calcular raiz cuadrada de un numero negativo"); 
+// }
+// Console.WriteLine("sen(" + num + ") = " + Math.Sin(num));
+// Console.WriteLine("cos(" + num + ") = " + Math.Cos(num));
+// Console.WriteLine("Parte entera de " + num + " = " + Math.Truncate(num));
+
+// Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~");
+
+// Console.Write("Ingrese el numero 1: ");
+// float num1 = solicitarNumero();
+// Console.Write("Ingrese el numero 2: ");
+// float num2 = solicitarNumero();
+
+// if (num1 > num2)
+// {
+//     Console.WriteLine("Maximo: " + num1);
+//     Console.WriteLine("Minimo: " + num2);
+// }
+// else if (num2 > num1)
+// {
+//     Console.WriteLine("Maximo: " + num2);
+//     Console.WriteLine("Minimo: " + num1);
+// }
+// else
+// {
+//     Console.WriteLine("Los dos numeros son iguales");
+// }
+// EJERCICIO 4
+string cadena, cadena2;
+
+Console.Write("Ingrese una cadena: ");
+cadena = Console.ReadLine();
+
+Console.WriteLine("Longitud de la cadena: " + cadena.Length);
+
+Console.Write("Ingrese una segunda cadena: ");
+cadena2 = Console.ReadLine();
+
+Console.WriteLine("Cadena 1 + Cadena 2 = " + cadena + cadena2);
+
+Console.WriteLine("Substring de cadena1: " + cadena.Substring(2, 2));
+
+foreach (var caracter in cadena)
 {
-    string numString = Console.ReadLine();
-    float num;
-
-    while (!float.TryParse(numString, out num))
-    {
-        Console.Write("Ingrese un numero valido");
-        numString = Console.ReadLine();
-    }
-    return num;
+    Console.WriteLine(caracter);
 }
-Console.Write("Ingrese un numero: ");
-float num = solicitarNumero();
 
-Console.WriteLine("|" + num + "| = " + Math.Abs(num));
-Console.WriteLine(num + "^2 = " + num * num);
-if (num >= 0)
+if (cadena.ToLower().Contains("hola"))
 {
-    Console.WriteLine("Raiz cuadrada de " + num + " = " + Math.Sqrt(num));
+    Console.WriteLine("\"hola\" se encuentra dentro de la cadena: " + cadena);
 }
 else
 {
-   Console.WriteLine("No podemos calcular raiz cuadrada de un numero negativo"); 
+    Console.WriteLine("\"hola\" no se encuentra dentro de la cadena: " + cadena);
 }
-Console.WriteLine("sen(" + num + ") = " + Math.Sin(num));
-Console.WriteLine("cos(" + num + ") = " + Math.Cos(num));
-Console.WriteLine("Parte entera de " + num + " = " + Math.Truncate(num));
 
-Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~");
+Console.WriteLine("Cadena en mayusculas: " + cadena.ToUpper());
+Console.WriteLine("Cadena en minusculas: " + cadena.ToLower());
 
-Console.Write("Ingrese el numero 1: ");
-float num1 = solicitarNumero();
-Console.Write("Ingrese el numero 2: ");
-float num2 = solicitarNumero();
+Console.Write("Ingrese el separador: ");
+char separador = Console.ReadLine()[0];
 
-if (num1 > num2)
+Console.Write("Ingrese la cadena a separar: ");
+string cadena3 = Console.ReadLine();
+
+Console.Write("[");
+foreach (var cad in cadena3.Split(separador))
 {
-    Console.WriteLine("Maximo: " + num1);
-    Console.WriteLine("Minimo: " + num2);
+    Console.Write($" {cad} ");
 }
-else if (num2 > num1)
-{
-    Console.WriteLine("Maximo: " + num2);
-    Console.WriteLine("Minimo: " + num1);
-}
-else
-{
-    Console.WriteLine("Los dos numeros son iguales");
-}
+Console.Write("]");
 
 
 
